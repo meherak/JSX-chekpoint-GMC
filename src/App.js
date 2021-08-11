c258da76050beccdacba82b8733bc2bfa2b72002
Meher Akermi <meher.akermi04@gmail.com>
2021-08-11 09:43:46 +0100

    missing style.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import srcImg from "./imageInSrc.jpg" ;
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">Your name here</h1>
+          <br />
+          <img src={srcImg} alt="srcimg" width={500} height={400}/>
+          <br />
+          <img src="/imageInPublic.jpg" alt="publicimg" width={500} height={400}/>
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
     </div>
   );
 }
